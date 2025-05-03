85a14874ee89bd1223cbc03212e2f9214d5ea9c8
dahemar <davidherreromarti@gmail.com>
2025-05-03 13:49:17 +0200

    d

--- convert_to_webp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Convierte todas las imágenes JPG, JPEG y PNG de ./images a WebP
-
-for img in ./images/*.{jpg,jpeg,png,JPG,JPEG,PNG}; do
-  [ -e "$img" ] || continue
-  cwebp -q 90 "$img" -o "${img%.*}.webp"
-done
-
-echo "Conversión a WebP completada." 
